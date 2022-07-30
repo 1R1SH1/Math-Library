5367886b6ac35ee5f27dad6e894165c1fdab1bf7
Rishat Murzyev <rmurzyev@mail.ru>
2022-07-30 17:27:44 +0500

    Добавьте файлы проекта.

---
+++ Method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class Method
+    {
+        public static double TriangleSquare(double A, double B, double C)
+        {
+            double p = (A + B + C) / 2;
+            double S = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return S;
+        }
+
+        public static double CircleSquare(double R)
+        {
+            return Math.PI * Math.Pow(R, 2);
+        }
+    }
+}
